1f264f5155b395fd80d69fbb38e35767c80964f4
Ifrim Silviu <silviu.redent@gmail.com>
2022-11-30 16:43:13 +0200

    Render basic list.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
-import { Box } from '@mui/material';
+import { Box, ListItem, ListItemButton, ListItemText, List, ListItemIcon } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
 function App() {
   return (
     <Box height="100vh" display="flex" alignItems="center" justifyContent="center">
-      Hello
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   )
 }
